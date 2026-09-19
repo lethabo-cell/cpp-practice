7fd0dc2708a9ff0b56353fbf458d89dc72214cf2
Lethabo <196253644+lethabo-cell@users.noreply.github.com>
2026-09-20 01:01:45 +0200

    updated calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a = 10;
-    int b = 20;
+    string operation;
+    double number1, number2;
 
-    cout << a + b <<endl;
+    cout<<"Enter First Number: ";
+    cin>>number1;
+    cout<<"Enter Operation(+, -, *, /): ";
+    cin>>operation;
+    cout<<"Enter Second Number: ";
+    cin>>number2;
 
+    if(operation == "+")
+    {
+        cout<< number1 + number2 << endl;
+    }
+    else if (operation == "-")
+    {
+        cout<< number1 - number2 << endl;
+    }
+    else if (operation == "*")
+    {
+        cout<< number1 * number2 << endl;
+    }
+    else if(operation == "/")
+    {
+        if(number2 != 0)
+        {
+            cout<< number1 / number2 <<endl;
+        }
+        else{
+            cout<<"Invalid: You cannot divide by a zero"<<endl;
+        }
+    }
+    else
+    {
+        cout<<"Not a recognised Operation"<<endl;
+    }
+    
     return 0;
 }
